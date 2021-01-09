c3e6532a4c0e57a4849f494ea344d138b6ce65f4
Rifki Alhuraibi <veelasky@gmail.com>
2021-01-09 10:39:20 +0700

    add basic packages

---
+++ config/routes.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Router configuration.
+ *
+ * @author      veelasky <veelasky@gmail.com>
+ */
+
+return [
+    'groups' => [
+        'web' => [
+            'middleware' => 'web',
+            'prefix' => '',
+        ],
+        'api' => [
+            'middleware' => 'api',
+            'prefix' => 'api',
+        ],
+    ],
+
+    'web' => [
+        /** @inject web **/
+    ],
+    'api' => [
+        /** @inject api **/
+    ],
+];
