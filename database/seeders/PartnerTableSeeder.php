0b24e1e1981f1ee0a1b3c953c5701b07760bfff4
airlanggaadie <airlangga_adie@yahoo.com>
2021-02-02 23:16:32 +0700

    TRAWLBENS-DEV-CR-5: TRAWLBENS-DEV-T-8: create Userable model. update data partner for testing. resolve transaction and commit query database. seeds users and validate partner for seeds transporter and warehouse.

--- database/seeders/PartnerTableSeeder.php
+++ database/seeders/PartnerTableSeeder.php
@@ -6,6 +6,10 @@ use League\Csv\Reader;
 use League\Csv\Statement;
 use Illuminate\Database\Seeder;
 use App\Models\Partners\Partner;
+use App\Models\Partners\Transporter;
+use App\Models\Partners\Warehouse;
+use App\Models\User;
+use App\Models\Userable;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
 use Symfony\Component\Console\Helper\ProgressBar;
@@ -23,9 +27,9 @@ class PartnerTableSeeder extends Seeder
     private ProgressBar $progressBar;
 
     /**
-     * @param mixed $filePath
-     * 
-     * @return [type]
+     * @param $filePath
+     * @return \Illuminate\Support\Collection
+     * @throws \League\Csv\Exception
      */
     public function loadFiles($filePath)
     {
@@ -42,38 +46,57 @@ class PartnerTableSeeder extends Seeder
     }
 
     /**
-     * Run the database seeds.
+     * Seed the application's database.
      *
      * @return void
+     * @throws \League\Csv\Exception
      */
     public function run()
     {
         $this->command->info('Load required files...');
-        // get data partner
         $this->partner = $this->loadFiles(__DIR__.'/data/partner.csv');
 
-        // showing progressbar
         $this->command->info('Populating partner data');
         $this->progressBar = $this->command->getOutput()->createProgressBar($this->partner->count());
 
-        DB::beginTransaction();
         foreach ($this->partner as $item) {
+            DB::beginTransaction();
             $this->progressBar->advance();
             $p = new Partner();
             $p->name = $item['name'];
             $p->address = $item['address'];
             $p->type = $this->modificationType($item['type']);
             $p->save();
-        }
-        DB::commit();
 
-        // finish seeding
+            $u = new User();
+            $u->name = $item['name'];
+            $u->email = $item['email'];
+            $u->password = "Trawlbens".$item['phonenumber']; //test data
+            $u->save();
+
+            $uable = new Userable();
+            $uable->user_id = $u->id;
+            $uable->partnerable()
+                ->associate($p)
+                ->save();
+
+            $this->validatePartner($p,$item);
+            DB::commit();
+        }
+
         $this->progressBar->finish();
         $this->command->newLine();
         $this->command->info('Finished populating partner data.');
     }
 
-    protected function modificationType($type)
+    /**
+     * Generate new type.
+     * 
+     * @param String $type
+     * 
+     * @return string
+     */
+    protected function modificationType(String $type) : string
     {
         switch ($type) {
             case 'WAREHOUSE':
@@ -84,6 +107,36 @@ class PartnerTableSeeder extends Seeder
                 return 'transporter';
             case 'SPACE':
                 return 'space';
+        }
+    }
+
+    /**
+     * Validate partner type for creating warehouse and transporter.
+     * 
+     * @param Partner $partner
+     * @param array $original
+     * 
+     * @return void
+     */
+    protected function validatePartner(Partner $partner, $original = []) : void
+    {
+        if (in_array($partner->type,['pool','space','business'])) {
+            $w = new Warehouse();
+            $w->partner_id = $partner->id;
+            $w->code = $original['code']; //data test
+            $w->name = $original['name']; //data test
+            $w->is_pool = $partner->type == 'pool' ? true : false;
+            $w->is_counter = in_array($partner->type,['business','space']) ? true : false;
+            $w->save();
+        }
+
+        if (in_array($partner->type,['business','transporter'])) {
+            $t = new Transporter();
+            $t->partner_id = $partner->id;
+            $t->name = $original['code']; //data test
+            $t->registration_number = 'stnk'; //data test
+            $t->type = 'bike'; //data test
+            $t->save();
         }
     }
 }
