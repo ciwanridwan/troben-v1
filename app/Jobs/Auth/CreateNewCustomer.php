000feca917d7caf99a26822dfa1a54da76be94d3
Rezky08 <Rezky08@users.noreply.github.com>
2021-02-03 03:09:48 +0000

    bot: cs fix

--- app/Jobs/Auth/CreateNewCustomer.php
+++ app/Jobs/Auth/CreateNewCustomer.php
@@ -9,7 +9,6 @@ use Illuminate\Queue\SerializesModels;
 use App\Events\Auth\NewCustomerCreated;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 
 class CreateNewCustomer
