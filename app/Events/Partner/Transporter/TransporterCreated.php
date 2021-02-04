ffd7ff0330b8a5f9054af371b40abb8680c85907
rezky08 <rezky.setiawan85@gmail.com>
2021-02-04 23:36:04 +0700

    TRAWLBENS-DEV-T-22: - remove unused script

--- app/Events/Partner/Transporter/TransporterCreated.php
+++ app/Events/Partner/Transporter/TransporterCreated.php
@@ -32,14 +32,4 @@ class TransporterCreated
     {
         $this->transporter = $transporter;
     }
-
-    /**
-     * Get the channels the event should broadcast on.
-     *
-     * @return \Illuminate\Broadcasting\Channel|array
-     */
-    public function broadcastOn()
-    {
-        return new PrivateChannel('channel-name');
-    }
 }
