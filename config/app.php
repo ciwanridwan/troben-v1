9e2a0817a278c1d0fb04c0ac582740e80a73e573
Rifki Alhuraibi <veelasky@pm.me>
2021-02-08 19:09:31 +0700

    update package

--- config/app.php
+++ config/app.php
@@ -165,6 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
+        NotificationChannels\Qontak\QontakServiceProvider::class,
 
         /*
          * Application Service Providers...
