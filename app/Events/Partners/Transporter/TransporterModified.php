dc7917c619d96dc57fe7646efc3f204fac18173b
airlanggaadie <airlangga_adie@yahoo.com>
2021-02-08 08:41:54 +0700

    TRAWLBENS-DEV-T-38: - add TransporterFactory - add event modified and modification failed - jobs update existing Transporter - add type of transporter - updating test for transporter

---
+++ app/Events/Partners/Transporter/TransporterModified.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Events\Partners\Transporter;
+
+use App\Models\Partners\Transporter;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class TransporterModified
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Transporter instance.
+     * 
+     * @var Transporter
+     */
+    public Transporter $transporter;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(Transporter $transporter)
+    {
+        $this->transporter = $transporter;
+    }
+}
