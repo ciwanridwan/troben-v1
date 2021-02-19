5bf27b129633dccc87dd1b4bad9219f99482ca7c
Rendy Ananta <rendyananta66@gmail.com>
2021-02-19 17:24:40 +0700

    TRAWLBENS-DEV-T-63: - add basic button

--- resources/antd-pro/pages/customer/customer-list.vue
+++ resources/antd-pro/pages/customer/customer-list.vue
@@ -1,10 +1,26 @@
 <template>
-  <div></div>
+  <a-card :title="title">
+    <create-button></create-button>
+    <detail-button></detail-button>
+    <edit-button></edit-button>
+    <delete-button></delete-button>
+  </a-card>
 </template>
 
 <script>
 export default {
-  name: "customer-list"
+  name: "customer-list",
+  props: {
+    cardTitle: {
+      type: String,
+      nullable: true
+    }
+  },
+  computed: {
+    title() {
+      return this.cardTitle || "Customer List"
+    }
+  }
 }
 </script>
 
