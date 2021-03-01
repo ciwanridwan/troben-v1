b75c26beb510519eaa6d11660e8c53aa3e5fed93
Rifki Alhuraibi <veelasky@pm.me>
2021-03-01 21:52:29 +0700

    remove order

---
+++ app/Models/Payments/Payment.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models\Payments;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Payment extends Model
+{
+    public const STATUS_PENDING = 'pending';
+    public const STATUS_CANCELLED = 'cancelled';
+    public const STATUS_FAILED = 'failed';
+    public const STATUS_SUCCESS = 'success';
+    public const STATUS_WAITING_FOR_APPROVAL = 'waiting_for_approval';
+
+    protected $table = 'payments';
+
+}
--- database/migrations/2021_01_23_110754_create_payments_table.php
+++ database/migrations/2021_01_23_110754_create_payments_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Payments\Payment;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
@@ -15,14 +16,25 @@ class CreatePaymentsTable extends Migration
     {
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
+            $table->string('service_type')->default('pay'); // pay, withdrawal, reversal.
             $table->string('payable_type');
             $table->unsignedBigInteger('payable_id');
             $table->string('channel')->default('bank_transfer');
+
             $table->decimal('payment_amount', 14, 2);
             $table->decimal('payment_admin_charges', 14, 2)->default(0);
+            $table->string('payment_ref_id')->nullable();
             $table->decimal('total_payment', 14, 2);
-            $table->string('status')->default('pending');
+            $table->string('status')->default(Payment::STATUS_PENDING);
+            $table->unsignedBigInteger('confirmed_by')->nullable(); // null => auto approve by system.
+            $table->timestamp('confirmed_at')->nullable();
             $table->timestamps();
+
+            $table->foreign('confirmed_by')
+                ->references('id')
+                ->on('users')
+                ->onDelete('set null');
+
         });
     }
 
