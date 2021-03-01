90282dda29c47cb5e508db34932e4215cd4fbd2c
airlanggaadie <airlangga_adie@yahoo.com>
2021-03-01 15:12:44 +0700

    TRAWLBENS-DEV-T-88: - new migration for inventories table

---
+++ database/migrations/2021_03_01_075416_create_inventories_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInventoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inventories', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('partner_id');
+            $table->string('name');
+            $table->float('capacity');
+            $table->float('height');
+            $table->integer('count');
+            $table->timestamps();
+
+            $table
+                ->foreign('partner_id')
+                ->references('id')
+                ->on('partners')
+                ->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inventories');
+    }
+}
