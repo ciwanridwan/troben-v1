7fe4a146a830203adae381c3d5736d007a12bc5d
rezky08 <rezky.setiawan85@gmail.com>
2021-03-03 13:24:31 +0700

    TRAWLBENS-DEV-T-96: - add route

---
+++ app/Http/Routes/Admin/Master/PaymentRoute.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Routes\Admin\Master;
+
+use App\Http\Controllers\Admin/Master/PaymentController;
+use Jalameta\Router\BaseRoute;
+
+class PaymentRoute extends BaseRoute
+{
+    /**
+     * Register routes handled by this class.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // make an awesome route
+    }
+
+    /**
+     * Controller used by this route.
+     *
+     * @return string
+     */
+    public function controller()
+    {
+        return Admin/Master/PaymentController::class;
+    }
+}
