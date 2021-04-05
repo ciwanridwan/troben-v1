4eb51f9f659a9a424566c11f17f16e2b9998e353
Rezky08 <Rezky08@users.noreply.github.com>
2021-04-05 10:19:26 +0000

    bot: cs fix

--- app/Http/Routes/VariableBindingRoute.php
+++ app/Http/Routes/VariableBindingRoute.php
@@ -5,8 +5,8 @@ namespace App\Http\Routes;
 use App\Models\Packages\Item;
 use Jalameta\Router\BaseRoute;
 use App\Models\Packages\Package;
-use App\Models\Deliveries\Delivery;
 use App\Models\Partners\Partner;
+use App\Models\Deliveries\Delivery;
 use App\Models\Partners\Transporter;
 use App\Models\Partners\Pivot\UserablePivot;
 
