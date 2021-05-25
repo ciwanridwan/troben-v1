2d3bc46ba263db2e98636af9dfe231eda5e3a6d8
rezky08 <rezky.setiawan85@gmail.com>
2021-05-25 16:00:23 +0700

    [API]: - repair mass assigned from code to delivery

--- app/Jobs/Deliveries/Actions/ProcessFromCodeToDelivery.php
+++ app/Jobs/Deliveries/Actions/ProcessFromCodeToDelivery.php
@@ -11,11 +11,13 @@ use App\Models\Deliveries\Deliverable;
 use Illuminate\Support\Facades\Validator;
 use App\Events\Packages\PackageAttachedToDelivery;
 use App\Events\Deliveries\Deliverable\DeliverableItemCodeUpdate;
+use Illuminate\Database\Eloquent\Collection;
 
 class ProcessFromCodeToDelivery
 {
     private Delivery $delivery;
 
+    private Collection $codes;
     private Code $code;
     /**
      * @var mixed
@@ -45,7 +47,8 @@ class ProcessFromCodeToDelivery
         ])->validate();
 
         /** @noinspection PhpFieldAssignmentTypeMismatchInspection */
-        $this->code = Code::query()->where('content', $inputs['code'])->with('codeable')->first();
+        $this->codes = Code::query()->whereIn('content', is_array($inputs['code']) ? $inputs['code'] : [$inputs['code']])->with('codeable')->get();
+
         $this->status = $inputs['status'];
     }
 
@@ -56,29 +59,38 @@ class ProcessFromCodeToDelivery
             $this->delivery->save();
         }
 
-        /** @var Package $package */
-        $package = $this->code->codeable instanceof Package ? $this->code->codeable : $this->code->codeable->package;
 
-        if ($this->delivery->packages()->where('id', $package->id)->doesntExist()) {
-            $this->delivery->packages()->attach($package);
+        $this->codes->each(function (Code $code) {
+            $this->code = $code;
 
-            $itemCodes = $package->items->pluck('codes')->flatten(1);
+            /** @var Package $package */
+            $package = $this->code->codeable instanceof Package ? $this->code->codeable : $this->code->codeable->package;
 
-            $this->delivery->item_codes()->syncWithoutDetaching($itemCodes->map->id->toArray());
 
-            event(new PackageAttachedToDelivery($package, $this->delivery));
-        }
+            if ($this->delivery->packages()->where('id', $package->id)->doesntExist()) {
 
-        if ($this->code->codeable instanceof Item && $this->status) {
-            $this->delivery->item_codes()->updateExistingPivot($this->code->id, [
-                'status' => $this->status,
-                'is_onboard' => Deliverable::isShouldOnBoard($this->status),
-            ]);
 
-            /** @var Code $code */
-            $code = $this->delivery->item_codes()->find($this->code->id);
+                $this->delivery->packages()->attach($package);
 
-            event(new DeliverableItemCodeUpdate($code->pivot));
-        }
+                $itemCodes = $package->items->pluck('codes')->flatten(1);
+
+                $this->delivery->item_codes()->syncWithoutDetaching($itemCodes->map->id->toArray());
+
+                event(new PackageAttachedToDelivery($package, $this->delivery));
+            }
+
+            if ($this->code->codeable instanceof Item && $this->status) {
+
+                $this->delivery->item_codes()->updateExistingPivot($this->code->id, [
+                    'status' => $this->status,
+                    'is_onboard' => Deliverable::isShouldOnBoard($this->status),
+                ]);
+
+                /** @var Code $code */
+                $code = $this->delivery->item_codes()->find($this->code->id);
+
+                event(new DeliverableItemCodeUpdate($code->pivot));
+            }
+        });
     }
 }
