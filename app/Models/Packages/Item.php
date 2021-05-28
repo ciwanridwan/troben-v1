6bf385b1c64651279372e5f971e5ec35bd9774e9
rezky08 <rezky.setiawan85@gmail.com>
2021-05-28 15:21:49 +0700

    [API] - add validation if out of range

--- app/Models/Packages/Item.php
+++ app/Models/Packages/Item.php
@@ -146,9 +146,12 @@ class Item extends Model
         $origin_province_id = $this->package->origin_regency->province_id;
         $origin_regency_id = $this->package->origin_regency_id;
         $destination_id = $this->package->destination_sub_district_id;
-
-        $price = PricingCalculator::getPrice($origin_province_id, $origin_regency_id, $destination_id);
-        $tierPrice = PricingCalculator::getTier($price, $this->WeightBorne);
+        try {
+            $price = PricingCalculator::getPrice($origin_province_id, $origin_regency_id, $destination_id);
+            $tierPrice = PricingCalculator::getTier($price, $this->WeightBorne);
+        } catch (\Throwable $th) {
+            $tierPrice = 0;
+        }
         return $tierPrice;
     }
 }
