713b41f6bed2b870b15769591220ef763ad8cdbb
rezky08 <rezky.setiawan85@gmail.com>
2021-06-07 02:56:49 +0700

    [COMPONENT] - add component for splitted modal - add component for manual tracking

---
+++ resources/antd-pro/components/trawl-manual-tracking.vue
@@ -0,0 +1,47 @@
+<template>
+  <trawl-modal-split>
+    <template slot="trigger">
+      <a-button class="trawl-button-success">Ubah posisi</a-button>
+    </template>
+    <template slot="title">
+      Ubah detail posisi
+    </template>
+    <template slot="left">
+      <a-space direction="vertical">
+        <span class="trawl-text-bold">No Manifest : MNF000202</span>
+        <a-space>
+          <car-icon />
+          <span>Budi Pratiwi - Xenia</span>
+        </a-space>
+      </a-space>
+    </template>
+    <template slot="rightHeader">
+      <a-space direction="vertical" :style="{ width: '100%' }">
+        <span class="trawl-text-bold">Posisi Terakhir</span>
+        <a-input-search placeholder="Posisi Terakhir" />
+      </a-space>
+    </template>
+    <template slot="rightContent">
+      <a-timeline>
+        <a-timeline-item>Create a services site 2015-09-01</a-timeline-item>
+        <a-timeline-item
+          >Solve initial network problems 2015-09-01</a-timeline-item
+        >
+        <a-timeline-item>Technical testing 2015-09-01</a-timeline-item>
+        <a-timeline-item
+          >Network problems being solved 2015-09-01</a-timeline-item
+        >
+      </a-timeline>
+    </template>
+    <template slot="rightFooter">
+      <a-button block class="trawl-button-success">Simpan</a-button>
+    </template>
+  </trawl-modal-split>
+</template>
+<script>
+import { CarIcon } from "./icons";
+import trawlModalSplit from "./trawl-modal-split.vue";
+export default {
+  components: { trawlModalSplit, CarIcon }
+};
+</script>
---
+++ resources/antd-pro/components/trawl-modal-split.vue
@@ -0,0 +1,88 @@
+<template>
+  <div>
+    <span class="trawl-modal-split--trigger" @click="showModal">
+      <slot name="trigger"></slot>
+    </span>
+    <a-modal
+      class="trawl-modal-split--modal"
+      :visible="visible"
+      :footer="null"
+      width="60%"
+      centered
+    >
+      <template slot="closeIcon">
+        <a-icon type="close" @click="hideModal"></a-icon>
+      </template>
+      <template slot="title">
+        <h3
+          class="trawl-modal-split--title trawl-text-normal trawl-text-bolder"
+        >
+          <slot v-if="hasSlot('title')" name="title"></slot>
+          <span v-else>Trawl Modal Split Title</span>
+        </h3>
+      </template>
+      <a-row type="flex" justify="space-between">
+        <a-col
+          class="trawl-modal-split--left-container"
+          :xs="24"
+          :sm="24"
+          :md="12"
+          :lg="12"
+          :xl="12"
+        >
+          <slot name="left"></slot>
+        </a-col>
+        <a-col
+          class="trawl-modal-split--right-container"
+          :xs="24"
+          :sm="24"
+          :md="12"
+          :lg="12"
+          :xl="12"
+        >
+          <a-layout
+            class="trawl-modal-split--right-layout trawl-bg-transparent"
+          >
+            <a-layout-header
+              v-if="hasSlot('rightHeader')"
+              class="trawl-bg-transparent trawl-modal-split--right-layout--header"
+            >
+              <slot name="rightHeader"></slot>
+            </a-layout-header>
+            <a-layout-content
+              class="trawl-bg-transparent trawl-modal-split--right-layout--content"
+            >
+              <slot name="rightContent"></slot>
+            </a-layout-content>
+            <a-layout-footer
+              v-if="hasSlot('rightFooter')"
+              class="trawl-bg-transparent trawl-modal-split--right-layout--footer"
+            >
+              <slot name="rightFooter"></slot>
+            </a-layout-footer>
+          </a-layout>
+        </a-col>
+      </a-row>
+    </a-modal>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      visible: false
+    };
+  },
+  methods: {
+    hideModal() {
+      return (this.visible = false);
+    },
+    showModal() {
+      return (this.visible = true);
+    },
+    hasSlot(slotName) {
+      return !!this.$slots[slotName];
+    }
+  }
+};
+</script>
