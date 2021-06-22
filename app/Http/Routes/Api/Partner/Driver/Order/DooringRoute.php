180cfd58e163aeba83ecbc15d201924f7407561b
airlanggaadie <airlangga_adie@yahoo.com>
2021-06-22 10:57:23 +0700

    [API] Dooring Arrived at Partner Origin [API] Dooring Loading by Driver [API] Dooring Arrived at Dooring Point [API] Dooring Unloading by Driver [API] Update Api for MTM driver status [API] Update list order by MTM owner

---
+++ app/Http/Routes/Api/Partner/Driver/Order/DooringRoute.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Routes\Api\Partner\Driver\Order;
+
+use App\Http\Controllers\Api\Partner\Driver\Order\DooringController;
+use Jalameta\Router\BaseRoute;
+
+class DooringRoute extends BaseRoute
+{
+    protected $prefix = 'partner/driver/order/dooring/{delivery_hash}';
+
+    protected $name = 'api.partner.driver.order.dooring';
+
+    protected $middleware = [
+        'partner.type:business,transporter',
+        'partner.role:driver',
+        'partner.scope.role:driver',
+    ];
+
+    /**
+     * Register routes handled by this class.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->router->patch($this->prefix('arrived'), [
+            'as' => $this->name('arrived'),
+            'uses' => $this->uses('arrived'),
+        ]);
+
+        // driver load an items from customer
+        $this->router->patch($this->prefix('loaded'), [
+            'as' => $this->name('loaded'),
+            'uses' => $this->uses('loaded'),
+        ]);
+
+        // driver arrive at partner (warehouse)
+        $this->router->patch($this->prefix('finished'), [
+            'as' => $this->name('finished'),
+            'uses' => $this->uses('finished'),
+        ]);
+
+        $this->router->patch($this->prefix('unloaded'), [
+            'as' => $this->name('unloaded'),
+            'uses' => $this->uses('unloaded'),
+        ]);
+    }
+
+    /**
+     * Controller used by this route.
+     *
+     * @return string
+     */
+    public function controller()
+    {
+        return DooringController::class;
+    }
+}
