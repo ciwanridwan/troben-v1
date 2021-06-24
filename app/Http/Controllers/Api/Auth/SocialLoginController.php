8f9e6342cf71fa839243419734cf875870cf268b
Rezky08 <Rezky08@users.noreply.github.com>
2021-06-24 03:57:01 +0000

    bot: cs fix

--- app/Http/Controllers/Api/Auth/SocialLoginController.php
+++ app/Http/Controllers/Api/Auth/SocialLoginController.php
@@ -3,11 +3,7 @@
 namespace App\Http\Controllers\Api\Auth;
 
 use Illuminate\Http\Request;
-use App\Models\OneTimePassword;
-use Illuminate\Validation\Rule;
-use Illuminate\Http\JsonResponse;
 use App\Http\Controllers\Controller;
-use App\Actions\Auth\AccountAuthentication;
 
 class SocialLoginController extends Controller
 {
