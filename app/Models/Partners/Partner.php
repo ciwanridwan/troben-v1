77496dbae098fcdd6ce98c66bea69a1bca1a738b
trawlbens <63690424+trawlbens@users.noreply.github.com>
2021-06-29 12:15:00 +0700

    Update Partner.php

--- app/Models/Partners/Partner.php
+++ app/Models/Partners/Partner.php
@@ -16,7 +16,6 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations;
 use App\Models\Partners\Pivot\UserablePivot;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Veelasky\LaravelHashId\Eloquent\HashableId;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
