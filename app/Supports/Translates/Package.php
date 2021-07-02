d6405927824990c3ed5a205fd6c6b9882a6ddcd8
airlanggaadie <airlangga_adie@yahoo.com>
2021-07-02 09:45:35 +0700

    [SUPPORT] clean code [TEST] clean code

--- app/Supports/Translates/Package.php
+++ app/Supports/Translates/Package.php
@@ -4,11 +4,12 @@ namespace App\Supports\Translates;
 
 use App\Contracts\HasCodeLog;
 use App\Models\Packages\Package as PackagesPackage;
+use App\Models\Partners\Partner;
 
 class Package implements HasCodeLog
 {
     /**
-     * @var App\Models\Packages\Package
+     * @var PackagesPackage $package
      */
     public PackagesPackage $package;
     function __construct(PackagesPackage $package)
@@ -22,7 +23,7 @@ class Package implements HasCodeLog
     public function translate(): string
     {
         $packageDescriptionFormat = $this->getDescriptionFormat();
-        if (!$packageDescriptionFormat) {
+        if (empty($packageDescriptionFormat)) {
             return $this->package->status . '_' . $this->package->payment_status;
         }
         // throw_if(!$packageDescriptionFormat, Error::make(Response::RC_CODE_LOG_UNAVAILABLE));
@@ -35,7 +36,6 @@ class Package implements HasCodeLog
         $description = __($packageDescriptionFormat['description'], $packageDescriptionFormat['variable']);
 
         return $description;
-        return '';
     }
 
     /**
@@ -43,12 +43,12 @@ class Package implements HasCodeLog
      */
     public function getDescriptionFormat(): array
     {
-        foreach (PackagesPackage::getAvailableDescriptionFormat() as $key => $value) {
+        foreach (PackagesPackage::getAvailableDescriptionFormat() as $value) {
             if (in_array($this->package->status, $value['status']) && in_array($this->package->payment_status, $value['payment_status'])) {
                 return $value;
             }
         }
-        return false;
+        return array();
     }
 
     /**
--- tests/Casts/Code/Log/DescriptionTest.php
+++ tests/Casts/Code/Log/DescriptionTest.php
@@ -2,16 +2,10 @@
 
 namespace Tests\Casts\Code\Log;
 
-use App\Casts\Code\Log\Description as LogDescription;
-use App\Events\Packages\PackageCreated;
 use App\Models\Packages\Package;
-use App\Supports\Translates\Package as TranslatesPackage;
 use App\Supports\Translates\Translate;
-use Database\Seeders\Packages\AssignedPackagesSeeder;
 use Database\Seeders\Packages\CashierInChargeSeeder;
-use Database\Seeders\Packages\PackagesTableSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class DescriptionTest extends TestCase
