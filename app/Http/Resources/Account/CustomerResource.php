1656f35a349b618525ce272c3e9f0485c30bfb59
airlanggaadie <airlangga_adie@yahoo.com>
2021-07-14 10:02:13 +0700

    [API] update api /me

--- app/Http/Resources/Account/CustomerResource.php
+++ app/Http/Resources/Account/CustomerResource.php
@@ -20,7 +20,7 @@ class CustomerResource extends JsonResource
             'name' => $this->name,
             'email' => $this->email,
             'phone' => $this->phone,
-            'attachment' => $this->attachments()->first()->uri ?? ''
+            'avatar' => $this->attachments()->first()->uri ?? null
         ];
 
         $address = $this->addresses;
