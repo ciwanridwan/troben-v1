550e0a44c4bd412aad5306e22280293133993170
Mandreee <juliansyahmandree@gmail.com>
2021-08-16 11:02:30 +0700

    Fix Get Partner list

--- app/Http/Controllers/Api/Partner/PartnerController.php
+++ app/Http/Controllers/Api/Partner/PartnerController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers\Api\Partner;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Api\Partner\PartnerResource;
+use App\Http\Resources\Geo\RegencyResource;
+use App\Models\Geo\Regency;
 use App\Models\Partners\Partner;
 use App\Models\Partners\Transporter;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 
 class PartnerController extends Controller
 {
@@ -33,38 +36,43 @@ class PartnerController extends Controller
     public function list(Request $request): JsonResponse
     {
         $this->attributes = Validator::make($request->all(), [
-            'type' => ['required'],
-            'origin' => ['nullable'],
+           /* 'type' => ['nullable', Rule::in([
+                'bike'
+            ])],*/
+            'type' => 'required',
+            'origin' => 'nullable',
         ])->validate();
 
-        $partner = Partner::query()->whereHas('transporters', function (Builder $query) {
-            $query->where('type', 'like', $this->attributes['type']);
-        })
-        ->get();
-
-
-//        ->where('type','=', 'business')
-//        ->orWhere('type','=', 'pool')
-
-
-        return $this->jsonSuccess(PartnerResource::collection($partner));
+        return $this->getPartnerData();
     }
 
-    public function getDeliveriesQuery(): Builder
+    protected function getPartnerData(): JsonResponse
     {
-        $query = Partner::query();
 
-        if ($this->partner->type === Partner::TYPE_TRANSPORTER) {
-            $userable = $this->user->transporters->first();
-            $query->where('userable_id', $userable->pivot->id);
-        } else {
-            $query->where(fn (Builder $builder) => $builder
-                ->orWhere('partner_id', $this->partner->id)
-                ->orWhere('origin_partner_id', $this->partner->id));
+        $query = $this->getBasicBuilder(Partner::query());
+        $query->when(request()->has('type'), fn ($q) => $q->whereHas('transporters', function (Builder $query) {
+            $query->where('type', 'like', $this->attributes['type']);
+        }));
+        $query->when(request()->has('origin'), fn ($q) => $q->where('geo_regency_id', $this->attributes['origin']));
 
-            $this->resolveDeliveriesQueryByRole($query);
-        }
+        return $this->jsonSuccess(PartnerResource::collection($query->paginate(request('per_page', 15))));
+    }
 
-        return $query;
+    /**
+     * Get Basic Builder.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $builder
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    private function getBasicBuilder(Builder $builder): Builder
+    {
+        $builder->when(request()->has('id'), fn ($q) => $q->where('id', $this->attributes['id']));
+        $builder->when(
+            request()->has('q') and request()->has('id') === false,
+            fn ($q) => $q->where('name', 'like', '%'.$this->attributes['q'].'%')
+        );
+
+        return $builder;
     }
 }
