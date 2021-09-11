8e2725132ae3ae39a9072cd39a7c4f57c4470cf5
Mandreee <juliansyahmandree@gmail.com>
2021-09-11 14:38:46 +0700

    Fix Assign manifest to driver

---
+++ app/Events/Deliveries/Kurir/Pickup/DriverArrivedAtPickupPoint.php
@@ -0,0 +1,41 @@
+<?php
+
+
+namespace App\Events\Deliveries\Kurir\Pickup;
+
+
+use App\Models\Deliveries\Delivery;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class DriverArrivedAtPickupPoint
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * @var \App\Models\Deliveries\Delivery
+     */
+    public Delivery $delivery;
+
+    /**
+     * Create a new event instance.
+     *
+     * @param \App\Models\Deliveries\Delivery $delivery
+     */
+    public function __construct(Delivery $delivery)
+    {
+        $this->delivery = $delivery;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('channel-name');
+    }
+}
