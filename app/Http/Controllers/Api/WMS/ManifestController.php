269831e91fa7bcd7523ccafbb92a060f127fedbc
Mandreee <juliansyahmandree@gmail.com>
2021-09-15 13:16:00 +0700

    Fixing create manifest transit and dooring, also Fixing get list manifest

--- app/Http/Controllers/Api/WMS/ManifestController.php
+++ app/Http/Controllers/Api/WMS/ManifestController.php
@@ -4,9 +4,12 @@ namespace App\Http\Controllers\Api\WMS;
 
 use App\Http\Response;
 use App\Jobs\Deliveries\Actions\ProcessFromCodeToDelivery;
+use App\Models\Code;
 use App\Models\Deliveries\Deliverable;
+use App\Models\Packages\Package;
 use App\Models\Partners\Pivot\UserablePivot;
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 use App\Models\Deliveries\Delivery;
@@ -18,6 +21,9 @@ use App\Http\Resources\Api\Delivery\WarehouseManifestResource;
 
 class ManifestController extends Controller
 {
+    public Collection $codes;
+    public Code $code;
+
     public function index(Request $request, PartnerRepository $repository): JsonResponse
     {
         $partner = $repository->getPartner();
@@ -35,7 +41,7 @@ class ManifestController extends Controller
         if ($request->to == null){
             $request->to = Carbon::now();
         }
-
+        $query->when(request()->has('regency_id'), fn ($q) => $q->where('destination_regency_id', $request->regency_id));
         $query->when(request()->has('from'), fn ($q) => $q->whereBetween('created_at', [$request->from, $request->to]));
         $query->when(request()->has('type'), fn ($q) => $q->where('type', $request->type));
         $query->when(request()->has('code'), fn ($q) => $q->whereHas('code', function ($query) use ($request) {
@@ -55,27 +61,33 @@ class ManifestController extends Controller
      */
     public function store(Request $request, PartnerRepository $repository): JsonResponse
     {
-        $job = new CreateNewManifest($repository->getPartner(), $request->all());
-        $this->dispatchNow($job);
+        $manifested_code = [
+            'existed' => []
+        ];
+        $this->codes = Code::query()->whereIn('content', is_array($request['code']) ? $request['code'] : [$request['code']])->with('codeable')->get();
 
-        $job = new ProcessFromCodeToDelivery($job->delivery, array_merge($request->only(['code']), [
-            'status' => Deliverable::STATUS_PREPARED_BY_ORIGIN_WAREHOUSE,
-            'role' => UserablePivot::ROLE_WAREHOUSE
-        ]));
-        $this->dispatchNow($job);
-
-        dd($job);
-
-        if ($job == 'package ada yg manifested'){
-            $delivery = Delivery::find($job->delivery->id);
-            $delivery->forceDelete();
-
-            return (new Response(Response::RC_BAD_REQUEST, $job))->json();
+        foreach ($this->codes as $code) {
+            /** @var Package $package */
+            $package = $code->codeable instanceof Package ? $code->codeable : $code->codeable->package;
+            if ($package->status == Package::STATUS_MANIFESTED) {
+                array_push($manifested_code['existed'], $code->content);
+            }
         }
 
+        if (empty($manifested_code['existed'])){
+            $order = new CreateNewManifest($repository->getPartner(), $request->all());
+            $this->dispatchNow($order);
 
+            $job = new ProcessFromCodeToDelivery($order->delivery, array_merge($request->only(['code']), [
+                'status' => Deliverable::STATUS_PREPARED_BY_ORIGIN_WAREHOUSE,
+                'role' => UserablePivot::ROLE_WAREHOUSE
+            ]));
+            $this->dispatchNow($job);
+        }
+        else{
+            return (new Response(Response::RC_BAD_REQUEST, $manifested_code))->json();
+        }
         return (new Response(Response::RC_SUCCESS, $job->delivery))->json();
-
     }
 
     public function show(Delivery $delivery): JsonResponse
