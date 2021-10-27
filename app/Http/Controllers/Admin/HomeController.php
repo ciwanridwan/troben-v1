14ed370e2ab724f79742ae1554ef15b99f4cbad1
Mandreee <juliansyahmandree@gmail.com>
2021-10-27 10:37:56 +0700

    [ADMIN] Adding package details in Resi page and Adding package status cancel in Data Order

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -78,7 +78,7 @@ class HomeController extends Controller
             $this->getSearch($request);
 
             // dont show canceled order
-            $this->query->where('status', '!=', Package::STATUS_CANCEL);
+            // $this->query->where('status', '!=', Package::STATUS_CANCEL);
             $this->dataRelation($request);
             $this->query->orderBy('created_at', 'desc');
             // $this->query->whereDoesntHave('deliveries');
