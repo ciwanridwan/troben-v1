17336b43d3b37a4b28b97f67ed2c7e06771429ef
airlanggaadie <airlangga_adie@yahoo.com>
2022-01-12 19:56:25 +0700

    [ROUTE] new route api for support trawlbens.id [NOTIFICATION] send notification via telegram when prospective partner register

---
+++ app/Http/Controllers/Api/SupportController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Notifications\Telegram\TelegramMessages\Marketing\ProspectivePartner;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Notification;
+
+class SupportController extends Controller
+{
+    /**
+     * Send notification when prospect partner register from https://trawlbens.id.
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function register(Request $request): JsonResponse
+    {
+        Notification::send([$request->toArray()],new ProspectivePartner());
+        return $this->jsonSuccess();
+    }
+}
--- config/telegram.php
+++ config/telegram.php
@@ -4,5 +4,6 @@ return [
     'chat' => [
         'app_group' => env('TELEGRAM_APP_GROUP_ID', -1001404809797),
         'finance_group' => env('TELEGRAM_FINANCE_GROUP_ID', -1001404809797),
+        'new_partner_group' => env('TELEGRAM_NEW_PARTNER_GROUP_ID', -1001404809797)
     ],
 ];
