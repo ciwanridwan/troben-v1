6cb68fae30294521f2b202d017a035019f61c3af
Mandreee <juliansyahmandree@gmail.com>
2022-02-08 09:36:59 +0700

    [API] Fix load item by driver

--- app/Http/Controllers/Api/Partner/Driver/Order/TransitController.php
+++ app/Http/Controllers/Api/Partner/Driver/Order/TransitController.php
@@ -74,7 +74,7 @@ class TransitController extends Controller
             ->pluck('id')->toArray();
         $deliveries = Deliverable::select('delivery_id')
             ->where('deliverable_type', 'App\Models\Code')
-            ->where('status', 'load_by_driver')
+            ->where('status', 'prepared_by_origin_warehouse')
             ->whereHas('delivery', function($q) use ($repository) {
                 $q->where('partner_id', $repository->getPartner()->id);
             })
