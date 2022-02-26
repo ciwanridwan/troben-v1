9e1eee88481cf3bf7ffa3d46d8ae569f1a439ca4
airlanggaadie <airlanggaadie@users.noreply.github.com>
2022-02-26 04:48:52 +0000

    bot: cs fix

--- app/Http/Resources/CodeLogResource.php
+++ app/Http/Resources/CodeLogResource.php
@@ -13,17 +13,17 @@ class CodeLogResource extends JsonResource
     public function toArray($request): array
     {
         return [
-            "id" => $this->resource->id,
-            "code_id" => $this->resource->code_id,
-            "code_logable_type" => $this->resource->code_logable_type,
-            "code_logable_id" => $this->resource->code_logable_id,
-            "type" => $this->resource->type,
-            "showable" => $this->resource->showable,
-            "status" => $this->resource->status,
-            "description" => $this->resource->description,
-            "created_at" => $this->resource->created_at->format('d-m-Y'),
-            "updated_at" => $this->resource->updated_at,
-            "deleted_at" => $this->resource->deleted_at
+            'id' => $this->resource->id,
+            'code_id' => $this->resource->code_id,
+            'code_logable_type' => $this->resource->code_logable_type,
+            'code_logable_id' => $this->resource->code_logable_id,
+            'type' => $this->resource->type,
+            'showable' => $this->resource->showable,
+            'status' => $this->resource->status,
+            'description' => $this->resource->description,
+            'created_at' => $this->resource->created_at->format('d-m-Y'),
+            'updated_at' => $this->resource->updated_at,
+            'deleted_at' => $this->resource->deleted_at
         ];
     }
 }
