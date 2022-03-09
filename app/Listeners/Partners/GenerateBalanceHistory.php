ed7300a26a4c2f33da32dd1f50318b3a633a5667
Mandreee <Mandreee@users.noreply.github.com>
2022-03-09 06:47:35 +0000

    bot: cs fix

--- app/Listeners/Partners/GenerateBalanceHistory.php
+++ app/Listeners/Partners/GenerateBalanceHistory.php
@@ -655,12 +655,11 @@ class GenerateBalanceHistory
         if ($variant == '0') {
             $discount = 0;
             if ($this->package->prices->where('type', Price::TYPE_DISCOUNT)
-                ->where('description', Price::TYPE_SERVICE)->first()->amount){
+                ->where('description', Price::TYPE_SERVICE)->first()->amount) {
                 $discount = $this->package->prices->where('type', Price::TYPE_DISCOUNT)
                     ->where('description', Price::TYPE_SERVICE)->first()->amount;
             }
             $balance_service = $this->package->total_amount * $this->getServiceFee($type) - $discount;
-
         } else {
             $balance_service = $this->package->total_weight * $this->getServiceFee($type);
         }
