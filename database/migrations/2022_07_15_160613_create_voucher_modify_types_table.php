2e121a6c3174a7c380a2eddeb9e2820a71598296
ryanda <ryandaputra@gmail.com>
2022-07-15 16:08:45 +0700

    add voucher type handle

---
+++ database/migrations/2022_07_15_160613_create_voucher_modify_types_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVoucherModifyTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('vouchers', function (Blueprint $table) {
+            $table->string('type')->default('discount_service_percentage'); // discount_service_percentage, discount_service_nominal, free_pickup
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+}
