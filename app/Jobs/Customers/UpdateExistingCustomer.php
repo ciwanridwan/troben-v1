4edb7a68d60b2bbb05858bc26bfe668fc84c5a62
ryanda <ryandaputra@gmail.com>
2022-07-17 20:32:18 +0700

    notif on reff code customer

--- app/Jobs/Customers/UpdateExistingCustomer.php
+++ app/Jobs/Customers/UpdateExistingCustomer.php
@@ -11,6 +11,8 @@ use Illuminate\Support\Facades\Validator;
 use App\Events\Customers\CustomerModified;
 use Illuminate\Foundation\Bus\Dispatchable;
 use App\Events\Customers\CustomerModificationFailed;
+use App\Models\Partners\NotificationAgent;
+use Illuminate\Support\Facades\DB;
 
 class UpdateExistingCustomer
 {
@@ -71,10 +73,30 @@ class UpdateExistingCustomer
      */
     public function handle(): bool
     {
+        $agent = null;
         if (array_key_exists('referral_code', $this->attributes)) {
-            if (User::where('referral_code', $this->attributes['referral_code'])->first() == null) {
-                return $this->referral = 'failed';
+            $rc = $this->attributes['referral_code'];
+            if (User::where('referral_code', $rc)->first() == null) {
+                // fallback, find from agent
+                $agent = DB::table('agents')->where('referral_code', $rc)->first();
+                if (is_null($agent)) {
+                    return $this->referral = 'failed';
+                }
             }
+        }
+
+        // customer is first time input referral_code and agent exist
+        if (
+            ($this->customer->referral_code == null || $this->customer->referral_code == '')
+            && isset($this->attributes['referral_code'])
+            && ! is_null($agent)) {
+            NotificationAgent::create([
+                'type' => 'referral_cust',
+                'title' => 'telah join melalui referral code',
+                'message' => $this->customer->name,
+                'status' => 'sent',
+                'agent_id' => $agent->id,
+            ]);
         }
 
         collect($this->attributes)->each(fn ($v, $k) => $this->customer->{$k} = $v);
