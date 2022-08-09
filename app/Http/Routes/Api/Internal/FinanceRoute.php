22b4f86b4e3d6c99eadc5045420e4389a839b80f
ciwanridwan <ridwan.andalus79@gmail.com>
2022-08-09 20:02:00 +0700

    add attribute approved_at

--- app/Http/Routes/Api/Internal/FinanceRoute.php
+++ app/Http/Routes/Api/Internal/FinanceRoute.php
@@ -28,7 +28,66 @@ class FinanceRoute extends BaseRoute
      */
     public function register()
     {
-        /**Move to request route */
+        $this->router->get($this->prefix('/list'), [
+            'as' => $this->name('list'),
+            'uses' => $this->uses('list', FinanceController::class),
+        ]);
+
+        $this->router->get($this->prefix('/count/amount'), [
+            'as' => $this->name('countAmountDisbursment'),
+            'uses' => $this->uses('countAmountDisbursment', FinanceController::class),
+        ]);
+
+        $this->router->get($this->prefix('/count'), [
+            'as' => $this->name('countDisbursment'),
+            'uses' => $this->uses('countDisbursment', FinanceController::class),
+        ]);
+
+        $this->router->get($this->prefix('/find/partner'), [
+            'as' => $this->name('findByPartner'),
+            'uses' => $this->uses('findByPartner', FinanceController::class),
+        ]);
+
+        $this->router->get($this->prefix('/find/status'), [
+            'as' => $this->name('findByStatus'),
+            'uses' => $this->uses('findByStatus', FinanceController::class),
+        ]);
+
+        $this->router->get($this->prefix('/find/date'), [
+            'as' => $this->name('findByDate'),
+            'uses' => $this->uses('findByDate', FinanceController::class),
+        ]);
+
+        $this->router->get($this->prefix('/detail/{id}'), [
+            'as' => $this->name('detail'),
+            'uses' => $this->uses('detail', FinanceController::class),
+        ]);
+
+        $this->router->get($this->prefix('/detail/{id}/ajax'), [
+            'as' => $this->name('detailAjax'),
+            'uses' => $this->uses('detail', FinanceController::class),
+        ]);
+        
+        $this->router->post($this->prefix('/detail/{id}/approve'), [
+            'as' => $this->name('approve'),
+            'uses' => $this->uses('approve', FinanceController::class),
+        ]);
+
+        $this->router->get($this->prefix('/detail/{id}/find/receipt'), [
+            'as' => $this->name('findByReceipt'),
+            'uses' => $this->uses('findByReceipt', FinanceController::class),
+        ]);
+
+        $this->router->get($this->prefix('/list/partners'), [
+            'as' => $this->name('listPartners'),
+            'uses' => $this->uses('listPartners', FinanceController::class),
+        ]);
+
+        $this->router->get($this->prefix('/report'), [
+            'as' => $this->name('report'),
+            'uses' => $this->uses('reportReceipt', FinanceController::class),
+        ]);
+
     }
 
     /**
