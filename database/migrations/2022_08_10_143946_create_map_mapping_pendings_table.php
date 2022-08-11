f76f3587c80400e1b55b6110abe2b773eb38ee3e
ryanda <ryandaputra@gmail.com>
2022-08-11 10:03:57 +0700

    add map mapping table

---
+++ database/migrations/2022_08_10_143946_create_map_mapping_pendings_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('map_mapping_pendings', function (Blueprint $table) {
+            $table->id();
+            $table->string('level');
+            $table->string('google_name');
+            $table->string('lat');
+            $table->string('lon');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('map_mapping_pendings');
+    }
+};
