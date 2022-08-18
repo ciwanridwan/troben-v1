7faafe8b64a56633b5aae4832c1143a4ed09a16c
ciwanridwan <ridwan.andalus79@gmail.com>
2022-08-18 18:04:06 +0700

    delete attributes customer_id

--- app/Http/Controllers/Api/Order/MotorBikeController.php
+++ app/Http/Controllers/Api/Order/MotorBikeController.php
@@ -34,23 +34,16 @@ class MotorBikeController extends Controller
         ];
 
         $request->validate([
-            'customer_id' => 'required|numeric',
             'service_code' => 'required|in:tps',
             'transporter_type' => 'required|in:bike,mvp,pickup,engkel box,cde engkel bak,pickup box,cdd double box,fuso bak,cdd double bak,',
             
             'sender_name' => 'required|string',
             'sender_phone' => 'required|string',
             'sender_address' => 'required',
-            // 'origin_regency_id' => 'required|numeric',
-            // 'origin_district_id' => 'required|numeric',
-            // 'origin_sub_district_id' => 'required|numeric',
 
             'receiver_name' => 'required|string',
             'receiver_phone' => 'required|string',
             'receiver_address' => 'required',
-            // 'destination_regency_id' => 'required|numeric',
-            // 'destination_distric_id' => 'required|numeric',
-            // 'destination_sub_district_id' => 'required|numeric',
 
             'moto_type' => 'required|in:matic,kopling,gigi',
             'moto_merk' => 'required',
@@ -59,16 +52,17 @@ class MotorBikeController extends Controller
             'moto_photo' => 'required|image',
             'moto_price' => 'required|numeric',
             
-            'items.*.qty' => 'required',
-            'items.*.name' => 'required',
-            'items.*.desc' => 'required',
-            'items.*.weight' => 'required',
-            'items.*.height' => 'required',
-            'items.*.length' => 'required',
-            'items.*.width' => 'required',
+            'items.*.qty' => 'nullable',
+            'items.*.name' => 'nullable',
+            'items.*.desc' => 'nullable',
+            'items.*.weight' => 'nullable',
+            'items.*.height' => 'nullable',
+            'items.*.length' => 'nullable',
+            'items.*.width' => 'nullable',
+            'items.*.insurance' => 'nullable',
             'items.*.is_insured' => 'nullable|boolean',
             'items.*.price' => 'required_if:*.is_insured,true|numeric',
-            'items.*.handling.*' => 'required|in:'.implode(',', $handlers),
+            'items.*.handling.*' => 'nullable:|in:'.implode(',', $handlers),
 
             'partner_code' => 'required',
             'origin_lat' => 'required',
