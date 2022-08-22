20bd1f02806dab869c35822ecf962c5ac9e0ed3a
ryanda <ryandaputra@gmail.com>
2022-08-22 08:12:01 +0700

    comment old func

--- app/Http/Controllers/Api/Order/OrderController.php
+++ app/Http/Controllers/Api/Order/OrderController.php
@@ -295,6 +295,7 @@ class OrderController extends Controller
         )));
     }
 
+    // deprecated, move to MotorBikeController
     public function storeMotorbike(Request $request): JsonResponse
     {
         $handlers = [
