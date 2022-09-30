cd53fe1a9827196d0bcce812bef03fa3e2047e27
ryanda <ryandaputra@gmail.com>
2022-09-30 14:03:43 +0700

    fix note

--- app/Http/Controllers/Api/Order/OrderController.php
+++ app/Http/Controllers/Api/Order/OrderController.php
@@ -167,7 +167,7 @@ class OrderController extends Controller
         /**Set condition for retrieve type by motobikes or items */
         if ($package['motoBikes'] !== null) {
             $result['type'] = 'bike';
-            $result['notes'] = $bikePrice->notes;
+            $result['notes'] = $bikePrice->notes ?? '';
             $result['packing_price'] = $package->prices()->where('type', PackagePrice::TYPE_HANDLING)->where('description', PackagePrice::DESCRIPTION_TYPE_BIKE)->get()->sum('amount');
             $result['packing_additional_price'] = $package->prices()->where('type', PackagePrice::TYPE_HANDLING)->where('description', PackagePrice::DESCRIPTION_TYPE_WOOD)->get()->sum('amount');
         } else {
