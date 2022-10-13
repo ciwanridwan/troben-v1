46c9fa21ea1489d4482d52631339bb1afcab21d7
ryanda <ryandaputra@gmail.com>
2022-10-13 11:01:24 +0700

    add info note

--- resources/antd-pro/components/packages/package-address.vue
+++ resources/antd-pro/components/packages/package-address.vue
@@ -9,6 +9,7 @@
       {{ sender_address }}
     </p>
     <span :style="textStyle"> Kode pos : {{ sender_zip_code }} </span>
+    <span :style="textStyle"> <strong>Note :</strong> {{ sender_note }} </span>
   </a-space>
   <a-space v-else direction="vertical" :size="0.1">
     <span v-if="title" :style="{ 'font-size': '.7rem' }"> Penerima </span>
@@ -22,6 +23,7 @@
       {{ receiver_address }}
     </p>
     <span :style="textStyle"> Kode pos : {{ receiver_zip_code }} </span>
+    <span :style="textStyle"> <strong>Note :</strong> {{ receiver_note }} </span>
   </a-space>
 </template>
 <script>
@@ -68,6 +70,9 @@ export default {
     receiver_name() {
       return this.package?.receiver_name;
     },
+    receiver_note() {
+      return this.package?.receiver_way_point;
+    },
 
     sender_zip_code() {
       return this.package?.origin_sub_district?.zip_code;
@@ -80,6 +85,9 @@ export default {
     },
     sender_name() {
       return this.package?.sender_name;
+    },
+    sender_note() {
+      return this.package?.sender_way_point;
     },
   },
 };
