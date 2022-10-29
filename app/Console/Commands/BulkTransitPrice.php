536a6481f381810b17d5121562a18f6b1d771596
ciwanridwan <ridwan.andalus79@gmail.com>
2022-10-29 13:56:25 +0700

    fix command inserting transit price

--- app/Console/Commands/BulkTransitPrice.php
+++ app/Console/Commands/BulkTransitPrice.php
@@ -8,12 +8,18 @@ use Illuminate\Support\Facades\DB;
 
 class BulkTransitPrice extends Command
 {
+    // Declare some constanta
+    private const TYPE_MTAK_1 = '1';
+    private const TYPE_MTAK_2 = '2';
+    private const TYPE_MTAK_3 = '3';
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'price:transit';
+    protected $signature = 'price:transit
+                            {type : MTAK Type 1, 2 Or 3 (possible values: 1,2,3)}
+                            {--F|file= : File path location, the type must be csv}';
 
     /**
      * The console command description.
@@ -21,6 +27,10 @@ class BulkTransitPrice extends Command
      * @var string
      */
     protected $description = 'Bulk data transit price (insert or update)';
+
+    private string $type;
+
+    private string $file_path;
 
     /**
      * Create a new command instance.
@@ -39,20 +49,31 @@ class BulkTransitPrice extends Command
      */
     public function handle()
     {
-        $file = database_path('csv/mtak_1_bandung.csv');
         $header = [
             'id', 'prov', 'kota_kab', 'kec', 'kel', 'zipcode', 'vendor_1', 'vendor_2', 'vendor_3',
             'tier_1', 'tier_2', 'tier_3', 'tier_4', 'tier_5', 'tier_6', 'tier_7', 'tier_8', "ket"
         ];
-        // dd($header);
-        $f = \Illuminate\Support\Facades\File::exists($file);
+
+        $this->type = $this->argument('type');
+        if (! in_array($this->type, self::getAvailableType())) {
+            $this->error('Wrong type argument');
+            return;
+        }
+
+        $this->file_path = $this->option('file');
+        if (! $this->file_path) {
+            $this->error('Option file is required');
+            return;
+        }
+
+        $f = \Illuminate\Support\Facades\File::exists($this->file_path);
         if (!$f) {
             $this->info('file not found');
             return;
         }
 
         $rows = [];
-        $data = $this->csv_to_array($file, $header);
+        $data = $this->csv_to_array($this->file_path, $header);
         foreach ($data as $k => $d) {
             if ($k == 0) continue;
 
@@ -70,23 +91,14 @@ class BulkTransitPrice extends Command
 
             $r = $z[0];
 
-            $rows[] = [
-                'origin_regency_id' => 1,
-                'destination_regency_id' => $r->regency_id,
-                'destination_district_id' => $r->id,
-                'type' => 1,
-                'vendor' => $d['vendor_1'],
-                'tier_1' => $d['tier_1'],
-                'tier_2' => $d['tier_2'],
-                'tier_3' => $d['tier_3'],
-                'tier_4' => $d['tier_4'],
-                'tier_5' => $d['tier_5'],
-                'tier_6' => $d['tier_6'],
-                'tier_7' => $d['tier_7'],
-                'tier_8' => $d['tier_8'],
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now()
-            ];
+            if ($this->type === self::TYPE_MTAK_1) {
+                $rows[] = $this->firstType($r, $d);
+            } elseif ($this->type === self::TYPE_MTAK_2) {
+                $rows[] = $this->secondType($r, $d);
+            } elseif ($this->type === self::TYPE_MTAK_3) {
+                $rows[] = $this->thirdType($r, $d);
+            }
+
 
             if (count($rows) == 100) {
                 try {
@@ -102,24 +114,111 @@ class BulkTransitPrice extends Command
             DB::table('partner_transit_prices')->insert($rows);
             $this->info(count($rows));
         }
+
+        $this->info('Update transit price finished.');
     }
 
-    private function csv_to_array($filename='', $header)
+    private function csv_to_array($filename = '', $header)
     {
-        $delimiter=',';
-        if(!file_exists($filename) || !is_readable($filename))
+        $delimiter = ',';
+        if (!file_exists($filename) || !is_readable($filename))
             return FALSE;
 
         $data = array();
-        if (($handle = fopen($filename, 'r')) !== FALSE)
-        {
-            while (($row = fgetcsv($handle, 0, $delimiter)) !== FALSE)
-            {
+        if (($handle = fopen($filename, 'r')) !== FALSE) {
+            while (($row = fgetcsv($handle, 0, $delimiter)) !== FALSE) {
                 $data[] = array_combine($header, $row);
             }
             fclose($handle);
         }
 
         return $data;
+    }
+
+    /**
+     * @return string[]
+     */
+    private static function getAvailableType(): array
+    {
+        return [
+            self::TYPE_MTAK_1,
+            self::TYPE_MTAK_2,
+            self::TYPE_MTAK_3
+        ];
+    }
+
+    /**
+     * @return array
+     * This rows for inserting to tables
+     */
+    private function firstType($r, $d): array
+    {
+        return [
+            'origin_regency_id' => 1,
+            'destination_regency_id' => $r->regency_id,
+            'destination_district_id' => $r->id,
+            'type' => 1,
+            'vendor' => $d['vendor_1'],
+            'tier_1' => $d['tier_1'],
+            'tier_2' => $d['tier_2'],
+            'tier_3' => $d['tier_3'],
+            'tier_4' => $d['tier_4'],
+            'tier_5' => $d['tier_5'],
+            'tier_6' => $d['tier_6'],
+            'tier_7' => $d['tier_7'],
+            'tier_8' => $d['tier_8'],
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ];
+    }
+
+    /**
+     * @return array
+     * This rows for inserting to tables
+     */
+    private function secondType($r, $d): array
+    {
+        return [
+            'origin_regency_id' => 1,
+            'destination_regency_id' => $r->regency_id,
+            'destination_district_id' => $r->id,
+            'type' => 2,
+            'vendor' => $d['vendor_2'],
+            'tier_1' => $d['tier_1'],
+            'tier_2' => $d['tier_2'],
+            'tier_3' => $d['tier_3'],
+            'tier_4' => $d['tier_4'],
+            'tier_5' => $d['tier_5'],
+            'tier_6' => $d['tier_6'],
+            'tier_7' => $d['tier_7'],
+            'tier_8' => $d['tier_8'],
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ];
+    }
+
+    /**
+     * @return array
+     * This rows for inserting to tables
+     */
+    private function thirdType($r, $d): array
+    {
+        return [
+            'origin_regency_id' => 1,
+            'destination_regency_id' => $r->regency_id,
+            'destination_district_id' => $r->id,
+            'type' => 3,
+            'vendor' => $d['vendor_3'],
+            'tier_1' => $d['tier_1'],
+            'tier_2' => $d['tier_2'],
+            'tier_3' => $d['tier_3'],
+            'tier_4' => $d['tier_4'],
+            'tier_5' => $d['tier_5'],
+            'tier_6' => $d['tier_6'],
+            'tier_7' => $d['tier_7'],
+            'tier_8' => $d['tier_8'],
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ];
     }
 }
