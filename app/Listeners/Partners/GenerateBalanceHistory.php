057648c1a2c3ecceed007e112723e66aaf2a54c6
ciwanridwan <ciwanridwan@users.noreply.github.com>
2022-11-03 03:46:18 +0000

    bot: cs fix

--- app/Listeners/Partners/GenerateBalanceHistory.php
+++ app/Listeners/Partners/GenerateBalanceHistory.php
@@ -156,7 +156,7 @@ class GenerateBalanceHistory
                     $this->setPackage($package);
                     $variant = '1';
                     # total balance service > record service balance
-                    if (!$this->partner->get_fee_transit) {
+                    if (! $this->partner->get_fee_transit) {
                         break;
                     }
                     if ($this->countDeliveryTransitOfPackage() > 1) {
@@ -236,7 +236,7 @@ class GenerateBalanceHistory
                                         ->recordHistory();
                                 }
                             } else {
-                               $balancePickup = 0;
+                                $balancePickup = 0;
                             }
                         }
 
@@ -275,7 +275,7 @@ class GenerateBalanceHistory
                                 ->where('type', $tier)
                                 ->first();
 
-                            if (!$price || $price->value == 0) {
+                            if (! $price || $price->value == 0) {
                                 $job = new CreateNewFailedBalanceHistory($this->delivery, $this->partner);
                                 $this->dispatchNow($job);
                                 $payload = [
@@ -303,7 +303,7 @@ class GenerateBalanceHistory
                                 ->where('destination_regency_id', $this->delivery->destination_regency_id)
                                 ->where('type', PartnerPrice::TYPE_FLAT)
                                 ->first();
-                            if (!$price) {
+                            if (! $price) {
                                 $job = new CreateNewFailedBalanceHistory($this->delivery, $this->partner);
                                 $this->dispatchNow($job);
                                 $payload = [
@@ -387,7 +387,7 @@ class GenerateBalanceHistory
                     ->setPartner($this->transporter->partner)
                     ->setPackage($event->package);
 
-                if (!$this->partner->get_fee_dooring) {
+                if (! $this->partner->get_fee_dooring) {
                     break;
                 }
 
@@ -405,7 +405,7 @@ class GenerateBalanceHistory
                     ->where('type', $tier)
                     ->first();
 
-                if (!$price) {
+                if (! $price) {
                     $job = new CreateNewFailedBalanceHistory($this->delivery, $this->partner, $this->package);
                     $this->dispatchNow($job);
 
@@ -414,7 +414,7 @@ class GenerateBalanceHistory
                             'manifest_code' => $this->delivery->code->content,
                             'package_code' => $this->package->code->content,
                             'origin' => $this->partner->regency->name,
-                            'destination' => $this->package->destination_regency->name . ', ' . $this->package->destination_district->name . ', ' . $this->package->destination_sub_district->name,
+                            'destination' => $this->package->destination_regency->name.', '.$this->package->destination_district->name.', '.$this->package->destination_sub_district->name,
                             'package_weight' => $weight,
                             'partner_code' => $this->partner->code,
                             'type' => TransporterBalance::MESSAGE_TYPE_PACKAGE,
@@ -458,7 +458,7 @@ class GenerateBalanceHistory
 
         /** @var Template $notification */
         $notification = Template::query()->firstWhere('type', '=', Template::TYPE_PARTNER_BALANCE_UPDATED);
-        if (!is_null($owner->fcm_token)) {
+        if (! is_null($owner->fcm_token)) {
             return new PrivateChannel($owner, $notification);
         }
     }
