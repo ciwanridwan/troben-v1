4595a8671547267a9946b0cd9be540f5d6a79d81
ciwanridwan <ridwan.andalus79@gmail.com>
2022-11-03 10:44:36 +0700

    temporary changed row inserting

--- app/Console/Commands/BulkTransitPrice.php
+++ app/Console/Commands/BulkTransitPrice.php
@@ -91,7 +91,7 @@ class BulkTransitPrice extends Command
         foreach ($data as $k => $d) {
             if ($k == 0) continue;
 
-            if (($k % 1000) == 0) $this->info($k);
+            if (($k % 10000) == 0) $this->info($k);
 
             $q = "SELECT * FROM geo_districts WHERE name ILIKE '%s' LIMIT 1";
             $q = sprintf($q, pg_escape_string($d['kec']));
