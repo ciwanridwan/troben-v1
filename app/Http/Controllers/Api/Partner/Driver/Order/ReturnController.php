bdedb149b98c1e71aa81b14b112c96c419007439
helloirfanaditya <helloirfanaditya@gmail.com>
2022-12-13 20:54:38 +0700

    try catch in chatbox

--- app/Http/Controllers/Api/Partner/Driver/Order/ReturnController.php
+++ app/Http/Controllers/Api/Partner/Driver/Order/ReturnController.php
@@ -83,7 +83,11 @@ class ReturnController extends Controller
             'participant_id' => $delivery->driver->id,
             'customer_id' => $delivery->packages[0]->customer_id
         ];
-        Chatbox::endSessionDriverChatbox($param);
+        try {
+            Chatbox::endSessionDriverChatbox($param);
+        } catch (\Exception $th) {
+            report($th);
+        }
         return $this->jsonSuccess(DeliveryResource::make($delivery));
     }
 }
