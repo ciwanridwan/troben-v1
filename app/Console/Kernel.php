6cba80c10097ab99bfb21ce9de15306e39432741
ciwanridwan <ridwan.andalus79@gmail.com>
2022-12-14 15:43:50 +0700

    set to normal schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,10 +27,10 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('alert:two')->cron('1 00 */1 * *');
-        // $schedule->command('alert:tree')->cron('1 00 */2 * *');
-        $schedule->command('alert:two')->everyFiveMinutes();
-        $schedule->command('alert:tree')->everyTenMinutes();
+        $schedule->command('alert:two')->cron('1 00 */1 * *');
+        $schedule->command('alert:tree')->cron('1 00 */2 * *');
+        // $schedule->command('alert:two')->everyFiveMinutes();
+        // $schedule->command('alert:tree')->everyTenMinutes();
     }
 
     /**
