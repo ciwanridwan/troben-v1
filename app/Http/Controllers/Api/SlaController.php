77822fe0ff33db3498a19af2a1f810cccca79ca2
ryanda <ryandaputra@gmail.com>
2022-12-14 17:57:35 +0700

    sla func

--- app/Http/Controllers/Api/SlaController.php
+++ app/Http/Controllers/Api/SlaController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Actions\Core\SlaLevel;
 use App\Http\Controllers\Controller;
 use App\Http\Response;
 use App\Models\Partners\Balance\History;
@@ -58,149 +59,12 @@ class SlaController extends Controller
     /** Set alert level of SLA */
     public function setAlert()
     {
-        $this->levelTwoDeliveries();
-        $this->levelTreeDeliveries();
-        $this->levelTwoPackages();
-        $this->levelTreePackages();
-        $this->setPenaltyPackages();
-        $this->setPenaltyDeliveris();
+        SlaLevel::doSlaSetter();
 
-        $message = ['message' => 'Running Alert'];
+        $message = ['message' => 'Called SLA Setter'];
         Log::info('Alert Level Two And Tree Just Running Of SLA');
 
         return (new Response(Response::RC_SUCCESS, $message))->json();
-    }
-
-    /**
-     * Set alert level 2 for deliveries (manifest)
-     */
-    public function levelTwoDeliveries()
-    {
-        $q = "UPDATE partner_delivery_performances t
-        SET level = 2,
-            deadline = deadline + interval '24' hour,
-            updated_at = NOW()
-        WHERE 1=1
-            AND level = 1
-            AND status = 1
-            AND reached_at IS NULL
-            AND deadline < NOW()
-            and not exists (
-                select 1
-                from partner_delivery_performances
-                WHERE 1=1
-                AND level = 2
-                AND status = 1
-                AND delivery_id = t.delivery_id
-                AND partner_id  = t.partner_id
-            )";
-
-        $result = DB::statement($q);
-        return $result;
-    }
-
-    /**
-     * Set alert level 3 for deliveries
-     */
-    public function levelTreeDeliveries()
-    {
-        $q = "UPDATE partner_delivery_performances t
-        SET level  = 3,
-            deadline = deadline + interval '24' hour,
-            updated_at = NOW()
-        WHERE 1=1
-            AND level = 2
-            AND status = 1
-            AND reached_at IS NULL
-            AND deadline < NOW()
-            and not exists (
-                select 1
-                from partner_delivery_performances
-                WHERE 1=1
-                AND level = 3
-                AND status = 1
-                AND delivery_id = t.delivery_id
-                AND partner_id  = t.partner_id
-            )";
-
-        $result = DB::statement($q);
-        return $result;
-    }
-
-    /** Set alert level 2 for packages */
-    public function levelTwoPackages()
-    {
-        $q = "UPDATE partner_package_performances t
-        SET level = 2,
-            deadline = deadline + interval '24' hour,
-            updated_at = NOW()
-        WHERE 1=1
-            AND level = 1
-            AND status = 1
-            AND reached_at IS NULL
-            AND deadline < NOW()
-            and not exists (
-                select 1
-                from partner_package_performances
-                WHERE 1=1
-                AND level = 2
-                AND status = 1
-                AND package_id = t.package_id
-                AND partner_id  = t.partner_id
-            )";
-
-        $result = DB::statement($q);
-        return $result;
-    }
-
-    /** Set alert level 3 for packages */
-    public function levelTreePackages()
-    {
-        $q = "UPDATE partner_package_performances t
-        SET level = 3,
-            deadline = deadline + interval '24' hour,
-            updated_at = NOW()
-        WHERE 1=1
-            AND level = 2
-            AND status = 1
-            AND reached_at IS NULL
-            AND deadline < NOW()
-            and not exists (
-                select 1
-                from partner_package_performances
-                WHERE 1=1
-                AND level = 3
-                AND status = 1
-                AND package_id = t.package_id
-                AND partner_id  = t.partner_id
-            )";
-
-        $result = DB::statement($q);
-        return $result;
-    }
-
-    public function setPenaltyPackages()
-    {
-        $q = "UPDATE partner_package_performances t
-        SET status = 10,
-            updated_at = NOW()
-        WHERE 1=1
-            AND level = 3
-            AND status = 1
-            AND reached_at IS NULL
-            AND deadline < NOW()
-            and not exists (
-                select 1
-                from partner_package_performances
-                WHERE 1=1
-                AND level = 3
-                AND status = 10
-                AND reached_at is null
-                and deadline < now()
-            )";
-
-        $result = DB::statement($q);
-        return $result;
     }
 
     /**
