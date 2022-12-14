cf37eb93f61b1c43187b83473123fc8030c6caa2
ciwanridwan <ridwan.andalus79@gmail.com>
2022-12-14 17:57:53 +0700

    hotfix class not defined

--- app/Http/Controllers/Api/Partner/Driver/Order/DooringController.php
+++ app/Http/Controllers/Api/Partner/Driver/Order/DooringController.php
@@ -19,6 +19,7 @@ use App\Models\Deliveries\Deliverable;
 use App\Models\Deliveries\Delivery;
 use App\Models\Packages\Package;
 use App\Models\User;
+use App\Services\Chatbox\Chatbox;
 use Carbon\Carbon;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
