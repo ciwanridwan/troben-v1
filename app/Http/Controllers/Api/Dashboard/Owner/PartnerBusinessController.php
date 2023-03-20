aa60ee4a0767201554f43ef3d088e773e015d548
ciwanridwan <ridwan.andalus79@gmail.com>
2023-03-20 16:48:25 +0700

    add partner business controller

---
+++ app/Http/Controllers/Api/Dashboard/Owner/PartnerBusinessController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Api\Dashboard\Owner;
+
+use App\Http\Controllers\Controller;
+use App\Supports\Repositories\PartnerRepository;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class PartnerBusinessController extends Controller
+{
+    # todo income partner
+    /**
+     * 
+     */
+    public function income(Request $request, PartnerRepository $repository): JsonResponse
+    {
+         
+    } 
+}
