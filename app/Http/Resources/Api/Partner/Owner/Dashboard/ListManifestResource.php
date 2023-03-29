d74382cea40969840b93c0687d501665092a5cbc
ciwanridwan <ridwan.andalus79@gmail.com>
2023-03-29 15:01:27 +0700

    change response of list manifest

--- app/Http/Resources/Api/Partner/Owner/Dashboard/ListManifestResource.php
+++ app/Http/Resources/Api/Partner/Owner/Dashboard/ListManifestResource.php
@@ -14,6 +14,25 @@ class ListManifestResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        // return parent::toArray($request);
+        $sla = [
+            'level' => 1,
+            'is_late' => true,
+            'deadline_at' => '2023-03-16 18:00:00',
+            'late_at' => '2023-03-16 16:52:40',
+            'done_at' => '2023-03-16 16:52:40'
+        ];
+
+        return [
+            'type' => $this->type,
+            'status' => $this->status,
+            'created_at' => $this->created_at,
+            'hash' => $this->hash,
+            'code' => $this->code ? $this->code->only('content') : null,
+            'partner' => $this->partner ? $this->partner->only('code', 'name', 'hash') : null,
+            'origin_partner' => $this->origin_partner ? $this->origin_partner->only('code', 'name', 'hash') : null,
+            'transporter' => $this->transporter ? $this->transporter->only('hash', 'type', 'registration_name') : null,
+            'sla' => $sla
+        ];
     }
 }
