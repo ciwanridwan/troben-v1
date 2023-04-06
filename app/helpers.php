3cfc69d1558d13bdf37516f14a6f9e48d16f0f91
ciwanridwan <ridwan.andalus79@gmail.com>
2023-04-06 16:36:38 +0700

    hotfix undefined func helpers

--- app/helpers.php
+++ app/helpers.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 if (!function_exists("change_format_number")) {
     /**
      * To change format number from request input
@@ -12,5 +12,36 @@ if (!function_exists("change_format_number")) {
         }
 
         return $phone;
+    }
+}
+
+if (!function_exists('generateUrl')) {
+    function generateUrl(string $path, int $hours = 1)
+    {
+        $fileurl = Storage::disk('s3')->temporaryUrl($path, Carbon::now()->addHours($hours));
+
+        return $fileurl;
+    }
+}
+
+if (!function_exists('handleUpload')) {
+    function handleUpload(UploadedFile $file, string $path)
+    {
+        $original = $file->getClientOriginalName();
+
+        if (!$file->isValid()) {
+            throw new \Exception(sprintf("Upload file %s is not valid", $original), 100103);
+        }
+
+        try {
+            $filename = sprintf('%s-%s.%s', date('ymd'), md5(microtime(true)), $file->extension());
+            Storage::disk('s3')->putFileAs($path, $file, $filename);
+            $filepath = sprintf('%s/%s', $path, $filename);
+        } catch (\Exception $e) {
+            report($e);
+            $filepath = 'nopic.png';
+        }
+
+        return $filepath;
     }
 }
