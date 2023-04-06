e63d9dd26595bb4477be91d4896a710972505fdc
ciwanridwan <ridwan.andalus79@gmail.com>
2023-04-06 12:56:04 +0700

    add condition of list dooring packages

--- app/Http/Controllers/Api/Partner/Warehouse/Manifest/AssignableController.php
+++ app/Http/Controllers/Api/Partner/Warehouse/Manifest/AssignableController.php
@@ -17,6 +17,7 @@ use App\Supports\Repositories\PartnerRepository;
 use App\Http\Resources\Admin\Master\PartnerResource;
 use App\Http\Resources\Api\Assignable\DriverTransporterResource;
 use App\Http\Resources\Api\Assignable\PackageResource;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Validation\Rule;
 
 class AssignableController extends Controller
@@ -230,6 +231,12 @@ class AssignableController extends Controller
             if (!is_null($q->deliveryRoutes)) {
                 $partnerDooringId = $q->deliveryRoutes->partner_dooring_id;
                 if ($partnerIdFromDeliveries === $partnerId &&  $partnerDooringId === $partnerId) {
+                    return true;
+                }
+            } else {
+                $delivery = $q->deliveries->last();
+                $checkRoutes = Route::checkDooring($q, $delivery);
+                if ($checkRoutes) {
                     return true;
                 }
             }
