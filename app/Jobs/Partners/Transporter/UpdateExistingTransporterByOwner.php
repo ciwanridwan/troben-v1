19ff5b0072ba5721212cdaa2e08f1417ff981777
ciwanridwan <ridwan.andalus79@gmail.com>
2023-04-28 16:46:49 +0700

    hotfix status transporter

--- app/Jobs/Partners/Transporter/UpdateExistingTransporterByOwner.php
+++ app/Jobs/Partners/Transporter/UpdateExistingTransporterByOwner.php
@@ -69,14 +69,11 @@ public function __construct(Transporter $transporter, $inputs = [])
         $this->transporter->type = $this->attributes['type'] ?? $this->transporter->type;
         $this->transporter->registration_number = $this->attributes['registration_number'] ?? $this->transporter->registration_number;
         $this->transporter->chassis_number = $this->attributes['vehicle_number'] ?? $this->transporter->chassis_number;
-        $this->transporter->is_verified = $this->attributes['is_active'] ?? $this->transporter->is_verified;
         if ($this->attributes['is_active'] === '1') {
-            $this->attributes['verified_at'] = Carbon::now();
+            $this->transporter->verified_at = Carbon::now();
         }
 
-        if ($this->attributes['is_active'] === '0') {
-            # code...
-        }
+        $this->transporter->is_verified = $this->attributes['is_active'] ?? $this->transporter->is_verified;
 
         if (!empty($this->attributes['vehicler_reg'])) {
             $vehicleImage = handleUpload($this->attributes['vehicler_reg'], 'vehicle_identification');
