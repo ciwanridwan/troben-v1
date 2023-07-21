91898fa57af280bfeecf0b53c080ccd06e5c146b
ryanda <ryandaputra@gmail.com>
2023-07-21 22:07:52 +0700

    add notif deprecated

--- resources/antd-pro/views/trawl-admin-layout.blade.php
+++ resources/antd-pro/views/trawl-admin-layout.blade.php
@@ -6,6 +6,15 @@
             @stack('head-tools')
         </template>
         <template slot="content">
+            <h3
+                class="trawl-text-bolder trawl-text-danger"
+                :style="textStyle"
+                style="margin: 50px"
+                >
+Pengumuman Penting <br>                
+Mulai tanggal 1 Agustus 2023, dashboard lama akan dimatikan. <br>
+Mohon gunakan dashboard baru di alamat <a href="https://admin.trawlbens.com/">admin.trawlbens.com</a> sebagai penggantinya. 
+                </h3>
             @yield('content')
         </template>
         <template slot="footer">
