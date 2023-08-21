6bbcef1fe68ab37f47193cda193408ddd2bfd345
ciwanridwan <ridwan.andalus79@gmail.com>
2023-08-21 18:49:16 +0700

    fix err not loaded relation address

--- app/Http/Resources/Account/CustomerInfoResource.php
+++ app/Http/Resources/Account/CustomerInfoResource.php
@@ -31,12 +31,12 @@ class CustomerInfoResource extends JsonResource
         $address = $this->addresses;
 
         if ($address->count() > 0) {
-            $data['address'] = ($address->where('is_default', true)->first()->only('address'))['address'];
+            $data['address'] = $address->where('is_default', true)->first() ? $address->where('is_default', true)->first()->only('address') : null;
 
             if (is_null($data['address'])) {
-                $data['address'] = ($address->where('is_selected', true)->first()->only('address'))['address'];
+                $data['address'] = $address->where('is_selected', true)->first() ? $address->where('is_selected', true)->first()->only('address') : null;
             } else {
-                $data['address'] = ($address->first()->only('address'))['address'];
+                $data['address'] = $address->first()->only('address');
             }
         }
 
