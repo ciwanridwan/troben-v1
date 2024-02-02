fbc1b0baca69774402c51ffe98fb8ab770e56f01
ciwanridwan <ridwan.andalus79@gmail.com>
2024-02-02 15:33:27 +0700

    set actual balancepartner

--- app/Http/Controllers/Api/Internal/FinanceController.php
+++ app/Http/Controllers/Api/Internal/FinanceController.php
@@ -782,6 +782,7 @@ class FinanceController extends Controller
                 return (new Response(Response::RC_BAD_REQUEST))->json();
             }
 
+            $this->setActualBalance($disbursment->partner_id);
             return (new Response(Response::RC_UPDATED, $disbursment))->json();
         } else {
             return (new Response(Response::RC_BAD_REQUEST))->json();
@@ -1014,5 +1015,71 @@ class FinanceController extends Controller
         }
 
         return (new Response(Response::RC_SUCCESS, $result))->json();
+    }
+
+    /**
+     * set actual balance partner
+     */
+    public function setActualBalance($partnerId)
+    {
+        $query = "SELECT coalesce(SUM(amount), 0) as balance FROM
+        (
+        select
+            *
+        from
+            (
+            select
+                content receipt_income,
+                pbh.amount,
+                pbh.created_at
+            from
+                (
+                select
+                    package_id,
+                    SUM(
+                    case
+                        when partner_balance_histories.type not in ('penalty', 'discount', 'withdraw')
+                        then partner_balance_histories.balance
+                    else partner_balance_histories.balance * -1 end
+                    ) amount,
+                    MAX(partner_balance_histories.created_at) created_at
+                from
+                    partner_balance_histories
+                left join partners on
+                    partner_balance_histories.partner_id = partners.id
+                where
+                    partners.id = '$partnerId'
+                    and package_id is not null
+                group by
+                    package_id
+            ) pbh
+            left join codes c on
+                pbh.package_id = c.codeable_id
+                and
+                c.codeable_type = 'App\Models\Packages\Package'
+        ) income
+        left join lateral (
+            select
+                receipt receipt_disbursed
+            from
+                disbursment_histories
+            left join partner_balance_disbursement on
+                disbursment_histories.disbursment_id = partner_balance_disbursement.id
+            left join partners on
+                partner_balance_disbursement.partner_id = partners.id
+            where
+                partners.id = '$partnerId'
+        ) disbursed on
+            income.receipt_income = disbursed.receipt_disbursed
+        where
+            1 = 1
+            and receipt_disbursed is null
+        order by
+            income.created_at desc
+
+        ) ssss";
+
+        $actualBalance = collect(DB::select($query))->first();
+        Partner::query()->where('id', $partnerId)->update(['balance' => $actualBalance->balance]);
     }
 }
